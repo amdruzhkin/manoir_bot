a3bf6627ebb4c2ea5ca2a23dd7cefb4a25255bb6
a.druzhkin <amdruzhkin@gmail.com>
2023-10-30 19:09:45 +0300

    prototype

--- config.py
+++ config.py
@@ -1,1 +1,8 @@
+from aiogram import Dispatcher
+from aiogram.fsm.storage.memory import MemoryStorage
+from aiogram.utils.callback_answer import CallbackAnswerMiddleware
+
 BOT_TOKEN = '6931858066:AAFVkDeOkeYjsyLk0tVBiONZP2N1l5EObtY'
+CHANNEL_ID = -1002135088402
+dp = Dispatcher(storage=MemoryStorage())
+dp.callback_query.middleware(CallbackAnswerMiddleware())
--- handlers.py
+++ handlers.py
@@ -1,17 +1,22 @@
 from aiogram import Router
 from aiogram.types import Message
 from aiogram.filters import Command
+from quiz_manager import quiz_manager
 
 router = Router()
 
+@router.message(Command("quiz"))
+async def start_handler(msg: Message):
+    await quiz_manager.run(msg)
+
 @router.message(Command("start"))
 async def start_handler(msg: Message):
-    await msg.answer("Приветственное сообщение")
+    await msg.answer("Hello message")
 
 @router.message(Command("info"))
 async def start_handler(msg: Message):
-    await msg.answer("Информация об эвентах")
+    await msg.answer("Events information")
 
 @router.message(Command("help"))
 async def start_handler(msg: Message):
-    await msg.answer("Информация боте")
+    await msg.answer("Bot information")
